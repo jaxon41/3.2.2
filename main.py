368e90854fe4f20268eeece3a3d9e4d7cfe2ceec
jaxon41 <jaxon.e.sawyer@gmail.com>
2026-03-12 15:28:11 +0000

    3.2.2 Added Skleton and Post and first steps

---
+++ main.py
@@ -0,0 +1,9 @@
+#Activity 3.2.2 Step 7
+from Post import Post
+
+all_posts_archive = []
+
+# your code here
+username = input("enter your username: ")
+print(f"Your username is: {username}")
+print("Welcome to Computogram {username}!")
